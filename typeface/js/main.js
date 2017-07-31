d1c3c0648ba2d69a411dc64361734409d3dde908
Isabel Fang <saeriel@saeriel.wireless.yale.internal>
2017-07-31 14:57:06 -0400

    relativism, absurdism, and some other letters added

--- typeface/js/main.js
+++ typeface/js/main.js
@@ -3,12 +3,33 @@ $( document ).ready(function() {
 	$("#page1").mousedown(function(){
 		console.log("can");
 		$("#constructtext").fadeIn(400);
-		$("#constructivism").hide();
+		$(".thought").hide();
 	});
 	$("#page1").mouseup(function(){
 		console.log("done");
-		$("#constructivism").fadeIn(400);
+		$(".thought").fadeIn(400);
 		$("#constructtext").hide();
 	});
+	
+	$("#page2").mousedown(function() {
+		$("#relativetext").fadeIn(400);
+		$(".thought").hide();
+	});
+	$("#page2").mouseup(function(){
+		console.log("done");
+		$(".thought").fadeIn(400);
+		$("#relativetext").hide();
+	});
+	
+	$("#page3").mousedown(function() {
+		$("#absurdtext").fadeIn(400);
+		$(".thought").hide();
+	});
+	$("#page3").mouseup(function(){
+		console.log("done");
+		$(".thought").fadeIn(400);
+		$("#absurdtext").hide();
+	});
+	
    
 });
